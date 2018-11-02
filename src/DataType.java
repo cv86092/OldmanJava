6e7c0517c74616a3299f9b06fc90fba56393fa39
cv86092 <cv86092@gmail.com>
2018-11-02 16:51:43 +0800

    more datatype

--- src/DataType.java
+++ src/DataType.java
@@ -6,6 +6,16 @@ public class DataType {
 		System.out.println("HelloWorld");
 		byte i = -122;
 		System.out.println(i);
+		byte y = 0b011;
+		System.out.println(y);
+		int x = 0x10;
+		System.out.println(x);
+		int z = 100_100_1000;
+		System.out.println(z);
+		char h = '\u5289';
+		System.out.println(h);
+		double j =2.0E+6;
+		System.out.println(j);
 	}
 
 }
