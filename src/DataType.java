701c97cf6d29e3b0daf754a0340cd651ce05dbff
cv86092 <cv86092@gmail.com>
2018-11-02 16:30:08 +0800

    add type

--- src/DataType.java
+++ src/DataType.java
@@ -4,7 +4,8 @@ public class DataType {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HelloWorld");
-
+		byte i = -122;
+		System.out.println(i);
 	}
 
 }
