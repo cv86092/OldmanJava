338139d28acf4b1505cfbd72e1a266497a4ac3f0
cv86092 <cv86092@gmail.com>
2018-11-19 17:29:21 +0800

    sucessful capture 20181119 1729

--- src/captureonWindows.java
+++ src/captureonWindows.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import com.google.common.io.*;
 
@@ -14,12 +16,15 @@ public class captureonWindows {
 
 	public static void main(String[] args) throws IOException {
 		System.setProperty("webdriver.gecko.driver", "C:\\Users\\Michael\\AppData\\Roaming\\java\\geckodriver.exe");
-		WebDriver driver = new FirefoxDriver();
-		driver.get("http://www.ltn.com.tw/");
+		DesiredCapabilities dc = new DesiredCapabilities();
+		dc.setCapability("marionatte", false);
+		FirefoxOptions opt = new FirefoxOptions();
+		opt.merge(dc);
+		FirefoxDriver driver =  new FirefoxDriver(opt);
+		driver.get("https://www.sex.com");
         File scrFile = ( (TakesScreenshot) driver ).getScreenshotAs(OutputType.FILE);
-		// Now you can do whatever you need to do with it, for example copy somewhere
         FileUtils.copyFile(scrFile, new File("D:\\20181119.png"));
-        driver.quit();
+		driver.quit();
 	}
 
 }
