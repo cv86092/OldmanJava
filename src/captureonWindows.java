e73b83f533dea54f4c629c84f9744cc293f9d20f
cv86092 <cv86092@gmail.com>
2018-11-20 12:23:25 +0800

    keep the timeout code

--- src/captureonWindows.java
+++ src/captureonWindows.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
@@ -21,9 +22,26 @@ public class captureonWindows {
 		FirefoxOptions opt = new FirefoxOptions();
 		opt.merge(dc);
 		FirefoxDriver driver =  new FirefoxDriver(opt);
-		driver.get("https://www.sex.com");
-        File scrFile = ( (TakesScreenshot) driver ).getScreenshotAs(OutputType.FILE);
-        FileUtils.copyFile(scrFile, new File("D:\\20181119.png"));
+		driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+		driver.get("http://www.bot.com.tw/");
+        File scrFile1 = ( (TakesScreenshot) driver ).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(scrFile1, new File("D:\\20181120-1.png"));
+		driver.get("http://www.ltn.com.tw/");
+        File scrFile2 = ( (TakesScreenshot) driver ).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(scrFile2, new File("D:\\20181120-2.png"));
+		
+		driver.get("http://www.michaeljacksion.com/");
+        File scrFile3 = ( (TakesScreenshot) driver ).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(scrFile3, new File("D:\\20181120-3.png"));
+        
+		driver.get("http://www.cpbl.com.tw/");
+        File scrFile4 = ( (TakesScreenshot) driver ).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(scrFile4, new File("D:\\20181120-4.png"));
+        
+		driver.get("http://www.wix.com/");
+        File scrFile5 = ( (TakesScreenshot) driver ).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(scrFile5, new File("D:\\20181120-5.png"));
+        
 		driver.quit();
 	}
 
